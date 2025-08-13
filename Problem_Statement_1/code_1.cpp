e8112c06e849c11a841e1113c4ea40b30f824437
Champei <chaitanya3911@gmail.com>
2025-08-13 15:00:29 +0530

    Create code_1.cpp

---
+++ Problem_Statement_1/code_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+    int increase_by;
+    cin>>increase_by;
+
+    for (int i = 0; i < str.size(); i++) {
+        char ch = str[i];
+
+        if (ch >= 'a' && ch <= 'z') { // lowercase
+            ch = (ch - 'a' + increase_by) % 26 + 'a';
+        }
+        else if (ch >= 'A' && ch <= 'Z') { // uppercase
+            ch = (ch - 'A' + increase_by) % 26 + 'A';
+        }
+        
+
+        cout << ch ;
+    }
+
+    cout << endl;
+    return 0;
+}
